3d5fb77a8f351ba28844dab4cd8144efdf082ee2
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-16 01:36:23 +0000

    function practice in c++

---
+++ function.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+// void myFunction()
+// {
+//     cout << "Its my Function" << endl;
+// }
+
+// void funcTwo();
+// void strFunc(string firstName){
+//     cout<<firstName<<endl;
+// }
+
+// int funcThree(int a, int b){
+//     return a+b;
+// }
+
+// void swapFunc(int &a, int &b)
+// {
+//     int c = a;
+//     a = b;
+//     b = c;
+// }
+
+// void funcArrayParameter(int myArray[5]){
+//     for(int i=0; i<5; i++){
+//         cout<<myArray[i]<<endl;
+//     }
+// }
+
+int main(void)
+{
+    // cout<<"Hello World"<<endl;
+    // myFunction();
+    // funcTwo();
+    // strFunc("Kailash");
+    // cout<<funcThree(10, 20)<<endl;
+    // int sumNum = funcThree(100, 200);
+    // cout<<sumNum<<endl;
+    // int numOne = 10, numTwo = 20;
+    // cout << numOne << numTwo << endl;
+    // swapFunc(numOne, numTwo);
+    // cout << numOne << numTwo << endl;
+    // int userAge[5]={14,25,33,44,51};
+    // funcArrayParameter(userAge);
+}
+
+// void funcTwo()
+// {
+//     cout << "This is funcTwo function, this is declare function in above code and defination are there in current line" << endl;
+// };
