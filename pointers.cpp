05c8a9da319e4e259669e295a32edba04cd40e55
kailash kumar <kailash414243@gmail.com>
2024-01-19 20:33:00 +0530

    pointer practice

---
+++ pointers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    // int numOne = 20;
+    // int *numTwo = &numOne;
+    // cout << numOne << endl
+    //      << numTwo << endl;
+
+    // int myArray[2] = {1, 20};
+    // int *pointArrayElement = &myArray[0];
+    // cout << pointArrayElement << endl
+    //      << myArray[0] << endl;
+    // pointArrayElement++;
+    // cout<<*pointArrayElement<<endl;
+
+    // int numOne = 10;
+    // int *pointerOne = &numOne;
+    // int **pointerTwo = &pointerOne;
+    // int ***pointerThree = &pointerTwo;
+
+    // cout << *pointerOne << endl
+    //      << **pointerTwo << endl
+    //      << ***pointerThree << endl;
+}
