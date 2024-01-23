1a397d3eb00b1f14a7564ada93ed7cffd2af52a7
kailash kumar <kailash414243@gmail.com>
2024-01-23 07:46:09 +0530

    array practice

---
+++ array.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+#define ARRAY_SIZE 10
+
+int main(void)
+{
+    // int arrayOne[5] = {10, 20,30,40,50};
+    // cout << arrayOne[2] << endl;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << "Array " << i << " is : " << arrayOne[i] << endl;
+    // }
+
+    // int arrayOne[ARRAY_SIZE];
+    // cout << "Please enter 10 digits: ";
+    // for (int i = 0; i < ARRAY_SIZE; i++)
+    // {
+    //     cin >> arrayOne[i];
+    // }
+    // for (int j = 0; j < ARRAY_SIZE; j++)
+    // {
+    //     cout << arrayOne[j] << endl;
+    // }
+
+    // string userName = "Kailash Kumar";
+    // cout<<userName<<endl;
+    // cout<<sizeof(userName)<<endl;
+    // cout<<userName.length()<<endl;
+    // if(userName.length()>7){
+    //     cout<<"Please you can input above 7 chractor"<<endl;
+    // }else{
+    //     cout<<"Please you can input below 7 charactor"<<endl;
+    // }
+
+    // cout<<userName[1]<<endl;
+
+    // struct studentData
+    // {
+    //     string stuName;
+    //     string stuAddres;
+    //     int stuClass;
+    //     int stuRoll;
+    // };
+
+    // struct studentData studentDetails;
+    // studentDetails.stuName = "Aryan Singh";
+    // cout << studentDetails.stuName << endl;
+    typedef int integerType;
+    // integerType arrayOne[2] = {10, 20};
+    // integerType *pointerOne[2];
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     *pointerOne[i] = arrayOne[i];
+    // }
+    // cout<<*pointerOne[0]<<endl;
+
+    // integerType arrayTwo[3] = {100, 20,300};
+    // integerType *pointerArray[3];
+    // *pointerArray[0] = arrayTwo[0];
+    // *pointerArray[1] = arrayTwo[1];
+
+    // for (int i = 0; i < 3; i++)
+    // {
+
+    //     pointerArray[i] = &arrayTwo[i];
+    // }
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     cout << *pointerArray[i] << endl;
+    // }
+
+    // for (int i = 0; i < 2; i++)
+    // {
+
+    //     cout << *pointerArray[i] << endl;
+    // }
+    // cout << *pointerArray[0] << endl;
+    // cout << *pointerArray[1] << endl;
+}
