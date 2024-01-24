7eb298f2967cb170dafd10f4d57529a766715f14
kailash kumar <kailash414243@gmail.com>
2024-01-24 19:44:04 +0530

    string practice

---
+++ string.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    // string firstName = "Kailash";
+    // string lastName = "Kumar";
+    // string fullName;
+    // fullName = firstName.append(lastName);
+    // fullName = fullName.length();
+    // cout<<fullName<<endl;
+    // cout<<firstName.length()<<endl;
+
+    // start: // lebel for goto statement come from here
+    //     string userFirstName, userSecondName;
+    //     cout << "Please enter your first Name: ";
+    //     cin >> userFirstName;
+    //     cout << "Please enter your second Name: ";
+    //     cin >> userSecondName;
+    //     if (userFirstName.length() == 7 && userSecondName.length() == 5)
+    //     {
+    //         cout << userFirstName.append(userSecondName) << endl;
+    //     }
+    //     system("cls");
+    //     goto start; // go to lebel : start
+
+    // string userName = "Kailash";
+    // string oppositeName;
+    // for (int i = 7; i <7; i--)
+    // {
+    //     oppositeName = userName[i];
+    //     // userName[i];
+    //     // cout << userName << endl;
+    // }
+    // cout << oppositeName << endl;
+}
