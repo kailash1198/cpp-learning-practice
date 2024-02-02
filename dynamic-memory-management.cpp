3addcab838a8f789cfb87d41d11d7e616447070d
kailash kumar <kailash414243@gmail.com>
2024-02-02 06:51:42 +0530

    dynamic memory allocation

---
+++ dynamic-memory-management.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    // int *pointerOne = new int;
+    // if (!pointerOne)
+    // {
+    //     cout << "Memory not allocated" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Memory allocation success" << endl;
+    //     *pointerOne = 100;
+    // }
+    // cout << "The value of pointerOne = " << *pointerOne << endl;
+    // delete pointerOne;
+
+    // int *pointerTwo = new (nothrow) int[5]; // nothrow return NULL instead progeam error
+    // if (!pointerTwo)
+    // {
+    //     cout << "Memory allocation falied" << endl;
+    // }
+    // else
+    // {
+    //     cout << "memory allocated done" << endl;
+    //     for (int i = 0; i < 5; i++)
+    //     {
+    //         pointerTwo[i] = i;
+    //     }
+    //     for (int k = 0; k < 5; k++)
+    //     {
+    //         cout << "The number is " << pointerTwo[k] << endl;
+    //     }
+    // }
+
+    // delete[] pointerTwo;
+}
