ea313a7d99f8ed329d3481e0fd5913d75d84ab4c
kailash kumar <kailash414243@gmail.com>
2024-02-05 07:36:42 +0530

    basics of OOPS

---
+++ oops-basic.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+
+// Opps conceps types (Fundamentals and important ):
+// 1. Class
+// 2. Object
+// 3.Abstraction
+// 4. Inheritance
+// 5. Encapsulation
+// 6. Polymorphism
+
+// class car
+// {
+// public:
+//     string carName;
+//     string carModel;
+//     int modelYear;
+//     int carCode;
+//     int fuelCapacity;
+// };
+
+// class myClass
+// {
+// public:
+//     void myMethod();
+// };
+// // define method
+// void myClass::myMethod()
+// {
+//     cout << "Hello, This method is declare and define outside of classes";
+// }
+
+// class employee{
+//     public:
+//         string employeeName;
+//     private:
+//         int sallery;
+// };
+
+
+int main(void)
+{
+
+    // employee employeeData;
+    // employeeData.employeeName = "Employee One";
+    // employeeData.sallery = 10000; //error because this is private, you access inside only class, not outside of class you can access
+
+    // class myClass
+    // {
+    //     // create a class
+    // public: // access specifier
+    //     int myNum;
+    //     string myString;
+    // };
+
+    // // object created from a class
+    // // You can create multiple object using an one class
+    // myClass myObj;
+    // myClass myObjSecond;
+    // myClass myObjectThird;
+
+    // //access attributes
+    // myObj.myNum = 100;
+    // myObj.myString = "Kailash Kumar";
+
+    // cout<<myObj.myNum<<endl;
+    // cout<<myObj.myString<<endl;
+
+    // car car1;
+    // car car2;
+    // car car3;
+
+    // car1.carName = "Honda city";
+    // car1.carModel  = "Honda";
+    // car1.modelYear = 2023;
+    // car1.carCode = 123;
+    // car1.fuelCapacity = 25;
+
+    // car2.carName = "BRV hyundai";
+    // car2.carModel = "BRV";
+    // car2.modelYear = 2020;
+    // car2.carCode = 1234;
+    // car2.fuelCapacity = 50;
+
+    // car3.carName = "Ennova Maruti suzuki";
+    // car3.carModel = "Ennova";
+    // car3.modelYear = 2024;
+    // car3.carCode = 3465;
+    // car3.fuelCapacity = 23;
+
+    // ---- class methods ------
+    // method define inside of class
+    // class classOne{
+    //     public:
+    //         void myMethod(){
+    //             cout<<"Hello World, This is method function inside of class"<<endl;
+    //         };
+    //         string userName;
+    // };
+
+    // classOne myObjOne;
+    // myObjOne.myMethod();
+    // myObjOne.userName = "Kailash Kumar";
+    // cout<<myObjOne.userName<<endl;
+
+    // method define outside of class
+}
