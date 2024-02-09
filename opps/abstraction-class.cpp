d99ddc9c9910f508769532001e6be4a299c866bf
kailash kumar <kailash414243@gmail.com>
2024-02-09 14:19:19 +0530

    abstraction class practice

--- opps/abstraction-class.cpp
+++ opps/abstraction-class.cpp
@@ -5,9 +5,26 @@ using namespace std;
 class firstClass
 {
 private:
+public:
+    int numOne;
+    int numTwo;
+    virtual void sumNum() = 0;
 };
 
-int main(void)
+class secondClass : public firstClass
+{
+private:
+public:
+    void sumNum(int a, int b)
+    {
+        numOne = a;
+        numTwo = b;
+        int c = numOne + numTwo;
+        cout << "The sum c = " << c << endl;
+    }
+};
+
+main(void)
 {
     // abstraction class concept and examples(in abstract class we always need atleast 1 virtual function)
 }
