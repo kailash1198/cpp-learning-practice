fe077b817c3423ea12de58edb7984cf34abc5448
kailash kumar <kailash414243@gmail.com>
2024-02-09 13:54:25 +0530

    virtual function understanding

--- opps/virtual-function.cpp
+++ opps/virtual-function.cpp
@@ -2,6 +2,45 @@
 #include <string>
 using namespace std;
 
+class baseClass
+{
+private:
+    int numOne;
+    int numTwo;
+
+public:
+    virtual void greeting()
+    {
+        cout << "Welcome to program of base class" << endl;
+    }
+
+    void intro()
+    {
+        cout << "I am Kailash" << endl;
+    }
+};
+
+class derivedClass : public baseClass
+{
+private:
+    int a;
+
+public:
+    void greeting()
+    {
+        cout << "Welcome to program of Derived class" << endl;
+    }
+};
+
 int main(void)
 {
+
+    baseClass baseObj1;
+    derivedClass derivedObj1;
+    // baseObj1.greeting();
+    // derivedObj1.greeting();
+    baseClass *basePointer;
+    basePointer = &derivedObj1;
+    basePointer->greeting(); // output derived class function because we defined virtual function in base class
+    basePointer->intro();
 }
