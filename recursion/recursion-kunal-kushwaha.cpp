fd883761d9ef571bd0a3a1618e9e650f7d8bc1cd
kailash kumar <kailash414243@gmail.com>
2024-02-14 11:49:46 +0530

    Recrsion understanding

---
+++ recursion/recursion-kunal-kushwaha.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// int numPrint(int n)
+// {
+
+//     //base condition for checking value of n
+//     if (n == 50)
+//     {
+//         return 0;
+//     }
+
+//     cout << n << endl;
+
+//     return numPrint(n + 2); //update n values
+// };
+
+// int fibbonaciNumber(int num)
+// {
+
+//     if (num < 2)
+//     {
+//         return num;
+//     }
+
+//     return fibbonaciNumber(num - 1) + fibbonaciNumber(num - 2);
+// };
+
+int main(void)
+{
+
+    // cout << numPrint(10);
+
+    // cout<<fibbonaciNumber(4)<<endl;
+}
--- recursion/tempCodeRunnerFile.cpp
+++ recursion/tempCodeRunnerFile.cpp
@@ -1,2 +1,1 @@
-dl;
-    return numTotal = n + myFunc(n - 1);
+
