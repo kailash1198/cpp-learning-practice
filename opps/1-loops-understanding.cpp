f6fab8c21040b801744e0ec85439bede7127a826
kailash kumar <kailash414243@gmail.com>
2024-02-16 23:18:52 +0530

    Logical loops working practice

---
+++ opps/1-loops-understanding.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    // int n;
+    // cin >> n;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << i << endl;
+    // }
+
+    // for (int i = 1; i < 10; i++)
+    // {
+    //     for (int j = 1; j < 10; j++)
+    //     {
+    //         cout << i<<" " << j<<endl;
+    //     }
+    // }
+
+    // int myArray[5][3];
+    // for(int i=0; i<5; i++){
+    //     for(int j=0; j<3; j++){
+    //         cin>>myArray[i][j];
+    //     }
+    // }
+
+    // system("cls");
+    // cout<<"Your entered element is : "<<endl;
+
+    // for(int i=0; i<5; i++){
+    //     for(int j=0; j<3; j++){
+    //         cout<<myArray[i][j]<<" ";
+    //     }
+    // }
+
+        // int myArray[3][2][5];
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //         for (int k = 0; k < 5; k++)
+    //         {
+    //             cin >> myArray[i][j][k];
+    //         }
+    //     }
+    // }
+
+    // system("cls");
+    // cout<<"Your output: "<<endl;
+    // for(int i=0; i<3; i++){
+    //     for(int j=0; j<2; j++){
+    //         for(int k=0; k<5; k++){
+    //             cout<<myArray[i][j][k]<<" ";
+    //         }
+    //     }
+    // }
+}
