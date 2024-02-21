1c93e90aee9a502ed534d88bed6a5df4201a538b
kailash kumar <kailash414243@gmail.com>
2024-02-21 11:32:16 +0530

    encapsulation problem practice

---
+++ encapsulation/12-encapsulation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Develop a class called Car with private member variables for the make, model, and year of manufacture. Implement public member functions to set and get these attributes, display the details of the car, and check if it is a vintage car (manufactured more than 50 years ago).
+
+class car{
+
+};
+
+int main(void){
+
+}
