da2269e81c358677b298d0350fbbc7199210b101
kailash1198 <kailash414243@gmail.com>
2024-02-26 17:42:40 +0530

    loop practice

---
+++ opps/practice/1-practice.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+// Loops flow understanding and practice
+// While Loop
+int main(void)
+{
+    // int num = 100;
+    // while (num >= 0)
+    // {
+    //     cout << "Number : " << num << " ";
+    //     num--;
+    // };
+
+    // 1. Write a program to calculate the sum of all numbers from 1 to N, where N is provided by the user.
+
+    // int n;
+    // int i = 0;
+    // int sum = 0;
+    // cout << "Enter n : ";
+    // cin >> n;
+
+    // while (i <= n)
+    // {
+    //     sum += i;
+    //     i++;
+    // }
+    // cout << "N sum is : " << sum << endl;
+
+    // 2. Create a program to find the factorial of a given number using a loop.
+}
