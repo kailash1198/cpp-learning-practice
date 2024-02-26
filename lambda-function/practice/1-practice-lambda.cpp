9761b2cb9d0f996c5a3a144866d6c1a105ff3335
kailash1198 <kailash414243@gmail.com>
2024-02-26 16:22:33 +0530

    lambda function and functors prctice

--- lambda-function/practice/1-practice-lambda.cpp
+++ lambda-function/practice/1-practice-lambda.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(void){
-    
+// LAMBDA function practice
+auto calculateArea = [](int a, int b, string name) -> string
+{
+    if (name == "triangle")
+    {
+        return to_string(a + b); // return int
+    }
+    else
+    {
+        return name; // return string
+    }
+};
+
+int main(void)
+{
+    // Example usage
+    cout << calculateArea(3, 4, "triangle") << endl;
+    cout << calculateArea(0, 0, "square") << endl;
 }
