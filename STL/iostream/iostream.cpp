bbd2642d65f6f7d78ddf69d550e45f16418eecc8
kailash kumar <kailash414243@gmail.com>
2024-03-16 22:45:11 +0530

    iostream practice

---
+++ STL/iostream/iostream.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// Main function
+int main(void)
+{
+    int numOne = 10;
+    int numTwo = 10;
+    int numThree = 10;
+
+    cerr<<"This is error keyword"<<endl;
+    
+}
