0b84b6509cb9fd87087c3d7bf8c8ebcc473a72b4
kailash kumar <kailash414243@gmail.com>
2024-03-22 20:01:40 +0530

    practice

---
+++ friend-function/1-practice.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+// Friend Function
+class one
+{
+private:
+    int privateNum;
+
+protected:
+    int protectedNum;
+
+public:
+};
+
+class two
+{
+private:
+    int pvtNum;
+
+protected:
+    int protecNum;
+
+public:
+};
+
+int main(void)
+{
+}
