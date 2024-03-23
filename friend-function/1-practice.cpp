1a653f909ac13c70018264a2f118b9de4a4ab113
kailash kumar <kailash414243@gmail.com>
2024-03-23 19:41:24 +0530

    friend function practice

--- friend-function/1-practice.cpp
+++ friend-function/1-practice.cpp
@@ -1,28 +1,39 @@
 #include <iostream>
 using namespace std;
 // Friend Function
+
+// class one
 class one
 {
 private:
-    int privateNum;
-
-protected:
-    int protectedNum;
+    int numOne;
+    int numTwo;
 
 public:
+    friend void funcOne(one &myObj);
+    void funcTwo()
+    {
+        cout << "This is function two" << endl;
+    }
 };
 
+// class two
 class two
 {
 private:
-    int pvtNum;
-
-protected:
-    int protecNum;
+    int one;
+    int two;
 
 public:
 };
 
+void funcOne(one &myObj)
+{
+    myObj.numOne = 10;
+    cout << "Num One = " << myObj.numOne << endl;
+}
 int main(void)
 {
+    one objOne;
+    funcOne(objOne);
 }
