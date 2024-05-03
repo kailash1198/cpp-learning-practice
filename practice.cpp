83b76fd14cc94061c06ac3bf07be0a021a520bc4
kailash kumar <kailash414243@gmail.com>
2024-05-03 18:52:01 +0530

    practice

--- practice.cpp
+++ practice.cpp
@@ -2,33 +2,32 @@
 #include <string>
 using namespace std;
 
-int l = S.length(), ans;
-        if (l == 1) {
-            //checking if S is integer of size 1
-            if (isdigit(S[0]))
-                ans = 4;
-            else
-                ans = 1;
-        } else {
-            int flag = 0;
-            for (int i = 0; i < l; i++) {
-                //checking whether S is integer or float/double
-                if (S[i] == '.') {
-                    flag = 1;
-                    int j = l - i + 1;
-                    //checking whether S is double
-                    if (j > 7)
-                        ans = 8;
-                    else
-                        ans = 4;
-                }
-            }
-            if (!flag) ans = 4;
+// A step array is an array of integers where each element has a difference of at most k with its neighbor
+// . Given a key x, we need to find the index value of x if multiple elements exist,
+// and return the first occurrence of the key.
+
+int search(int arr[], int n, int x, int k)
+{
+    // Complete the function
+    int index;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            index = i;
         }
-        return ans;
+    }
+
+    return index;
+}
 
 int main()
 {
-    BasicDataType("ab");
+    int myArray[5] = {1, 2, 3, 4, 5};
+    int size=5;
+    int searchKey = 2;
+    int indexValue;
+
+    search(myArray, size, searchKey);
     return 0;
 }
