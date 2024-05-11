7db8a49caa6395635034a2a4166fa7acb2e94628
kailash kumar <kailash414243@gmail.com>
2024-05-11 23:01:36 +0530

    structure codebase

---
+++ structing-codebase/practice/2-include-guard.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "my-guards.h"
+#include "my-guards.h"
+#include "my-guards.h"
+#include "my-guards.h"
+
+int main(){
+    myFunction();
+    return 0;
+}
---
+++ structing-codebase/practice/my-guards.h
@@ -0,0 +1,8 @@
+#ifndef MY_GUARDS
+#define MY_GUARDS
+
+void myFunction(){
+    std::cout<<"Myfunction from my-guards.h called with help of header files seperate create -> for checking and working on include guards in codebase for resolve error if same header files decalre multiple time in main files"<<std::endl;
+}
+
+#endif
