085d0167232fc55cb17423a2330b09a85e0f8909
kailash kumar <kailash414243@gmail.com>
2024-05-11 11:53:36 +0530

    bitwise operation practice

---
+++ BITWISE/bitwise-operator/2-bitwise.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int numOne = 10;
+    int numTwo = 20;
+
+    if (numOne & numTwo)
+    {
+        std::cout << "True" << std::endl;
+    }
+    else
+    {
+        std::cout << "False" << std::endl;
+    }
+
+    return 0;
+}
